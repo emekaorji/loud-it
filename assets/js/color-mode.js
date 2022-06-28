feedc5f54da00d63b2a38c57618646d673a44954
Emeka Orji <emekapraiseo@gmail.com>
2022-06-28 16:53:52 +0100

    style: fine-tuned transition

--- assets/js/color-mode.js
+++ assets/js/color-mode.js
@@ -2,25 +2,33 @@ var isScrolling;
 const colorModeButton = document.getElementById('colorModeButton');
 
 colorModeButton.addEventListener('focus', function () {
+	colorModeButton.style.transition = 'all 300ms ease';
 	colorModeButton.style.opacity = 1;
 });
 colorModeButton.addEventListener('blur', function () {
+	colorModeButton.style.transition = 'all 2s ease, outline 500ms ease';
 	colorModeButton.style.opacity = 0.1;
 });
 colorModeButton.addEventListener('mouseover', function () {
+	colorModeButton.style.transition = 'all 300ms ease';
 	colorModeButton.style.opacity = 1;
 });
 colorModeButton.addEventListener('mouseout', function () {
-	colorModeButton.style.opacity = 0.1;
+	colorModeButton.style.transition = 'all 2s ease, outline 500ms ease';
+	if (document.activeElement !== colorModeButton) {
+		colorModeButton.style.opacity = 0.1;
+	}
 });
 
 window.addEventListener(
 	'scroll',
 	function () {
 		clearTimeout(isScrolling);
+		colorModeButton.style.transition = 'all 500ms ease';
 		colorModeButton.style.opacity = 0.8;
 
 		isScrolling = setTimeout(function () {
+			colorModeButton.style.transition = 'all 2s ease, outline 500ms ease';
 			colorModeButton.style.opacity = 0.1;
 		}, 1000);
 	},
